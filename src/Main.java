49156eadde020984faec144cd21f5339feca08ce
rajahUC <rajaho@mail.uc.edu>
2023-08-05 17:00:51 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        ArrayList<String> lines = new ArrayList<>();
+
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+
+                int line = 0;
+                int character = 0;
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    lines.add(rec);
+                    line++;
+                    character += rec.length();
+                }
+                reader.close();
+                System.out.println("Data file read!");
+
+                System.out.println("\nSummary Report: ");
+                System.out.printf("Name of the file: %s", selectedFile);
+                System.out.println("\nNumber of lines: " + line);
+
+                String[] words;
+                int word = 0;
+                for(String l:lines)
+                {
+                    words = l.split(" ");
+                    word += words.length;
+                }
+                System.out.println("Number of words: " + word);
+                System.out.println("Number of characters: " + character);
+            }
+
+            else
+            {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+        }
+
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
